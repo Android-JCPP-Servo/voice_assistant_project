f864473bafc453531d3c359ab9e53b298024c36e
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-10 15:23:02 -0600

    Add note and test function for mappings

--- main.py
+++ main.py
@@ -14,8 +14,17 @@ speaker.setProperty('rate', 150) # 150 is a good average rate
 # Create TODO list
 todo_list = ["Go shopping", "Clean Room", "Program R4-P17 Voice Assistant"]
 
+"""
+Mappings and Functions:
+If a mapping recognizes a specific command or request, it will pass an appropriate function
+"""
+def some_function():
+    print("Hello World!")
+
+mappings = {"greeting": some_function}
+
 # Set up assistant
-assistant = GenericAssistant('intents.json')
+assistant = GenericAssistant('intents.json', intent_methods=mappings)
 assistant.train_model()
 
 # Test assistant request
