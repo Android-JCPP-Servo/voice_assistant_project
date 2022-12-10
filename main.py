5d0b93d5953e495ec3fc7016493b39f63f153c59
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-10 15:14:02 -0600

    Initialize speech recognizer and speaker system

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ from neuralintents import GenericAssistant
 import speech_recognition
 import pyttsx3 as tts
 import sys
+
+# Set up a speech recognizer
+recognizer = speech_recognition.Recognizer()
+
+# Set up speaker
+speaker = tts.init()
+speaker.setProperty('rate', 150) # 150 is a good average rate
+
