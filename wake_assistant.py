9070b8bf9a102b70dacbfdef61923bd9b53951fc
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-15 21:32:22 -0600

    Create wake_assistant file with imported modules

---
+++ wake_assistant.py
@@ -0,0 +1,12 @@
+# Import internal modules
+import sys
+import threading
+import tkinter as tk
+
+# Import external modules
+import speech_recognition
+import pyttsx3 as tts
+
+# Import custom modules
+from neuralintents import GenericAssistant
+
