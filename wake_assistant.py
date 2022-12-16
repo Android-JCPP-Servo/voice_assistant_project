bc8e8bc9c6f73251e07285127a2e3b98516c39a4
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-15 21:46:46 -0600

    Initialize recognizer, speaker, and assistant

--- wake_assistant.py
+++ wake_assistant.py
@@ -10,3 +10,27 @@ import pyttsx3 as tts
 # Import custom modules
 from neuralintents import GenericAssistant
 
+# Create an assistant class
+class Assistant:
+
+    # Provide simple constructor
+    def __init__(self):
+
+        # Initialize recognizer with speech recognition
+        self.recognizer = speech_recognition.Recognizer()
+
+        # Initialize speaker
+        self.speaker = tts.init()
+        self.speaker.setProperty("rate", 150)
+
+        # Initialize assistant
+        self.assistant = GenericAssistant("intents.json", intent_methods={"file: self.create_file"})
+        self.assistant.train_model()
+
+        # 
+
+    # Method for creating a file
+    def create_file(self):
+
+        # Temporarily pass
+        pass
