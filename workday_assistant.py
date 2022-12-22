f38095e0c2db5ef3accf689c0b9205205f0536d6
Android-JCPP-Servo <astewart1138@gmail.com>
2022-12-22 00:11:57 -0600

    Set Jack to open all workday links at once

--- workday_assistant.py
+++ workday_assistant.py
@@ -1,6 +1,9 @@
+# Import webbrowser module
+import webbrowser
+
 # Import custom modules
 from open_links import open_work, open_youtube, open_stonks
-from play_sounds import play_command
+from play_sounds import play_response, play_command
 
 """
 WORKDAY ASSISTANT
@@ -10,6 +13,10 @@ def start_workday():
     # Let me know what you're doing, Jack!
     print("\nJack says:", "Opening your workday links now!\n")
     play_command()
-    open_work() # Open work link
-    open_youtube() # Open youtube
-    open_stonks() # Open stonks
+    # Set array of all links
+    workday_links = ['https://cyberlandr.a2hosted.com/web', 'https://www.youtube.com/', 'https://portfolio.primerica.com/']
+    # Loop through array and open links
+    for i in workday_links:
+        webbrowser.open_new(i)
+    # As Jack's opening links, play response voice and open all links at once...
+    play_response()
